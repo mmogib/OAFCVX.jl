af9ee8145ff15070cec7832da8fe7f01e68940b4
Documenter.jl <documenter@juliadocs.github.io>
2024-03-31 14:48:48 +0000

    build based on c2f3e0a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OAFCVX","category":"page"},{"location":"#OAFCVX","page":"Home","title":"OAFCVX","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OAFCVX.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OAFCVX]","category":"page"}]
+}
